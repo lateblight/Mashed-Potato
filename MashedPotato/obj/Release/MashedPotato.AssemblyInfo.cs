15f74c45fc7d1d6973a8d8330a6e41db3848defe
BuildTools <unconfigured@null.spigotmc.org>
2026-08-28 19:07:48 +0100

    Remove broken sln and correctly compile API level 15

--- MashedPotato/obj/Release/MashedPotato.AssemblyInfo.cs
+++ MashedPotato/obj/Release/MashedPotato.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("AGPL-3.0-or-later")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Mashes all Lalafells into the race of your choice.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+35d5f88770f6444395def67bef5c185069e402e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.0+a4632b23f25508c9b90cbf29e979e8d34af8573f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MashedPotato")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MashedPotato")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
